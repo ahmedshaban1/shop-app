6b5178e9f70e088a1640f4c33c56692906536bba
ahmedshaban1 <ahmed.shabaan.fcih@gmail.com>
2021-01-23 14:04:46 +0200

    init

--- app/src/main/java/com/shopapp/course/screens/products/presentation/ProductDetailsActivity.kt
+++ app/src/main/java/com/shopapp/course/screens/products/presentation/ProductDetailsActivity.kt
@@ -1,15 +1,19 @@
 package com.shopapp.course.screens.products.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Paint
 import android.os.Bundle
-import android.view.View
-import com.shopapp.course.R
-import com.shopapp.course.helpers.openActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.shopapp.course.databinding.ActivityProductDetailsBinding
+import com.shopapp.course.helpers.validateActionBar
 
 class ProductDetailsActivity : AppCompatActivity() {
+    lateinit var binding:ActivityProductDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_details)
+        binding = ActivityProductDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        validateActionBar("Black top")
+        binding.priceTvBefore.paintFlags = binding.priceTvBefore.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
 
     }
 }
